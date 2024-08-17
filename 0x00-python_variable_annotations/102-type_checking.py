9a515f6d66f656b4a2150ad695996dc54717f216
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-08-17 03:16:52 +0100

    type_checking.py

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pthon3
+"""
+Task 12
+"""
+
+from typing import List, Union
+
+def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
