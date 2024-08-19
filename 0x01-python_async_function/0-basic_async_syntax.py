9e619f7e7ee298b8b57e817dd81a77d5e115720d
Zamambo <122832552+ZamamboGcwabe@users.noreply.github.com>
2024-08-19 20:24:27 +0100

    Basic async syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,8 +8,8 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a random delay.
-    """
+    '''Waits for a random number of seconds.
+    '''
     wait_time = random.random() * max_delay
-    await asyncio.seep(wait_time)
+    await asyncio.sleep(wait_time)
     return wait_time
